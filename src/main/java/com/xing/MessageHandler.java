6b5f146cdf047c9844e572ecfaeac0513742c1cb
honxchen <honxchen@thoughtworks.com>
2020-06-07 18:24:07 +0800

    add metric

--- src/main/java/com/xing/MessageHandler.java
+++ src/main/java/com/xing/MessageHandler.java
@@ -1,16 +1,36 @@
 package com.xing;
 
+import com.amazonaws.services.cloudwatch.AmazonCloudWatch;
+import com.amazonaws.services.cloudwatch.AmazonCloudWatchClientBuilder;
+import com.amazonaws.services.cloudwatch.model.Dimension;
+import com.amazonaws.services.cloudwatch.model.MetricDatum;
+import com.amazonaws.services.cloudwatch.model.PutMetricDataRequest;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import org.slf4j.LoggerFactory;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+import java.util.Arrays;
 
 public class MessageHandler implements RequestHandler<String, String> {
     @Override
     public String handleRequest(String input, Context context) {
         LambdaLogger logger = context.getLogger();
-        logger.log("receive message: " + input);
-        return input;
+        AmazonCloudWatch cloudWatch = AmazonCloudWatchClientBuilder.standard().withRegion("ap-southeast-1").build();
+        AmazonS3 s3Client = AmazonS3ClientBuilder.standard().withRegion("ap-southeast-1").build();
+
+        double count = s3Client.listObjects("hongxing-deployment-bucket").getObjectSummaries().size();
+        PutMetricDataRequest request = new PutMetricDataRequest()
+                .withNamespace("/aws/lambda/hongxing")
+                .withMetricData(new MetricDatum()
+                        .withMetricName("honxing-custom-memory-metric")
+                        .withUnit("Count")
+                        .withValue(count));
+        cloudWatch.putMetricData(request);
+
+        logger.log("hongxing-deployment-bucket has " + count + " object");
+        return "hongxing-deployment-bucket has " + count + " object";
     }
 
 }
