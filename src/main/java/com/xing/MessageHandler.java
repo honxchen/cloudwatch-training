a800fc0c28241b6b5232565e3e1248f42888101a
honxchen <honxchen@thoughtworks.com>
2020-06-07 20:06:15 +0800

    add schedule event

--- src/main/java/com/xing/MessageHandler.java
+++ src/main/java/com/xing/MessageHandler.java
@@ -2,20 +2,18 @@ package com.xing;
 
 import com.amazonaws.services.cloudwatch.AmazonCloudWatch;
 import com.amazonaws.services.cloudwatch.AmazonCloudWatchClientBuilder;
-import com.amazonaws.services.cloudwatch.model.Dimension;
 import com.amazonaws.services.cloudwatch.model.MetricDatum;
 import com.amazonaws.services.cloudwatch.model.PutMetricDataRequest;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.ScheduledEvent;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
-import java.util.Arrays;
-
-public class MessageHandler implements RequestHandler<String, String> {
+public class MessageHandler implements RequestHandler<ScheduledEvent, String> {
     @Override
-    public String handleRequest(String input, Context context) {
+    public String handleRequest(ScheduledEvent input, Context context) {
         LambdaLogger logger = context.getLogger();
         AmazonCloudWatch cloudWatch = AmazonCloudWatchClientBuilder.standard().withRegion("ap-southeast-1").build();
         AmazonS3 s3Client = AmazonS3ClientBuilder.standard().withRegion("ap-southeast-1").build();
